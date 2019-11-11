6517da2c2f1610401669e77127e839b6d39f182e
Nina Choi <nina@choi.com.br>
2019-11-11 20:17:41 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace udemyAula2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bom dia !");
+            Console.WriteLine("Boa tarde !");
+
+            //se eu apenas escrever console.write o código não fará a quebra de linha, ficando em uma linha só, um do 
+            //lado do outro. MAS só funcina se o write estive antes do segundo Write Line
+            Console.Write("Bom dia !");
+            Console.WriteLine("Boa tarde !");
+        }
+    }
+}
